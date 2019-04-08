654e3861a76e5c8a93aab9d7a3e7cb6153a3fc6a
Stu <49402927+stuartkbird@users.noreply.github.com>
2019-04-08 16:39:34 +0100

    Area calculator

---
+++ AreaCalculator/AreaCalculator/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace AreaCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnCalculateArea = new System.Windows.Forms.Button();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Area";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(70, 6);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 1;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(70, 31);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 2;
+            // 
+            // btnCalculateArea
+            // 
+            this.btnCalculateArea.Location = new System.Drawing.Point(32, 96);
+            this.btnCalculateArea.Name = "btnCalculateArea";
+            this.btnCalculateArea.Size = new System.Drawing.Size(75, 41);
+            this.btnCalculateArea.TabIndex = 3;
+            this.btnCalculateArea.Text = "Calculate Area";
+            this.btnCalculateArea.UseVisualStyleBackColor = true;
+            this.btnCalculateArea.Click += new System.EventHandler(this.btnCalculateArea_Click);
+            // 
+            // lblArea
+            // 
+            this.lblArea.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblArea.Location = new System.Drawing.Point(72, 58);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(100, 23);
+            this.lblArea.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.btnCalculateArea);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Area Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnCalculateArea;
+        private System.Windows.Forms.Label lblArea;
+    }
+}
+
---
+++ AreaCalculator/AreaCalculator/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AreaCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateArea_Click(object sender, EventArgs e)
+        {
+            //declare variable (local scope)
+            int width;  //stores the width of the rectangle
+            int height; //stores the height of the rectangle
+            int area;   //stores the result of multiplying width by height
+
+            //read inputs
+            width = Convert.ToInt32(txtWidth.Text);
+            height = Convert.ToInt32(txtHeight.Text);
+
+            //process(do calculation)
+            area = width * height;
+
+            //display output
+            lblArea.Text = Convert.ToString(area);
+
+        }
+    }
+}
