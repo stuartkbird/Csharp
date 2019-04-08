1348d162a0923068c49094b53c2d476f65d891c9
Stu <49402927+stuartkbird@users.noreply.github.com>
2019-04-08 16:57:03 +0100

    Pong / Brick Breaker type game

---
+++ Simple Game Program/Simple Game Program/frmSimpleGame.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Simple_Game_Program
+{
+    partial class frmSimpleGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSimpleGame));
+            this.lblLivesLabel = new System.Windows.Forms.Label();
+            this.lblScoreLabel = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLife = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.imgPaddle = new System.Windows.Forms.PictureBox();
+            this.imgBall = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imgPaddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLivesLabel
+            // 
+            this.lblLivesLabel.AutoSize = true;
+            this.lblLivesLabel.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLivesLabel.Location = new System.Drawing.Point(12, 9);
+            this.lblLivesLabel.Name = "lblLivesLabel";
+            this.lblLivesLabel.Size = new System.Drawing.Size(54, 25);
+            this.lblLivesLabel.TabIndex = 0;
+            this.lblLivesLabel.Text = "Lives";
+            // 
+            // lblScoreLabel
+            // 
+            this.lblScoreLabel.AutoSize = true;
+            this.lblScoreLabel.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreLabel.Location = new System.Drawing.Point(514, 9);
+            this.lblScoreLabel.Name = "lblScoreLabel";
+            this.lblScoreLabel.Size = new System.Drawing.Size(58, 25);
+            this.lblScoreLabel.TabIndex = 1;
+            this.lblScoreLabel.Text = "Score";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Mistral", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Maroon;
+            this.lblName.Location = new System.Drawing.Point(211, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(150, 57);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Ricochet";
+            // 
+            // lblLife
+            // 
+            this.lblLife.AutoSize = true;
+            this.lblLife.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblLife.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLife.Location = new System.Drawing.Point(17, 39);
+            this.lblLife.Name = "lblLife";
+            this.lblLife.Size = new System.Drawing.Size(24, 27);
+            this.lblLife.TabIndex = 3;
+            this.lblLife.Text = "3";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblScore.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(548, 39);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(24, 27);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "0";
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.BackColor = System.Drawing.Color.White;
+            this.lblGameOver.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.Location = new System.Drawing.Point(238, 336);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(0, 25);
+            this.lblGameOver.TabIndex = 5;
+            // 
+            // imgPaddle
+            // 
+            this.imgPaddle.Image = ((System.Drawing.Image)(resources.GetObject("imgPaddle.Image")));
+            this.imgPaddle.Location = new System.Drawing.Point(260, 320);
+            this.imgPaddle.Name = "imgPaddle";
+            this.imgPaddle.Size = new System.Drawing.Size(50, 10);
+            this.imgPaddle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imgPaddle.TabIndex = 6;
+            this.imgPaddle.TabStop = false;
+            // 
+            // imgBall
+            // 
+            this.imgBall.BackColor = System.Drawing.Color.White;
+            this.imgBall.Image = ((System.Drawing.Image)(resources.GetObject("imgBall.Image")));
+            this.imgBall.Location = new System.Drawing.Point(47, 37);
+            this.imgBall.Name = "imgBall";
+            this.imgBall.Size = new System.Drawing.Size(10, 12);
+            this.imgBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imgBall.TabIndex = 7;
+            this.imgBall.TabStop = false;
+            this.imgBall.Click += new System.EventHandler(this.imgBall_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmSimpleGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.imgBall);
+            this.Controls.Add(this.imgPaddle);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblLife);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblScoreLabel);
+            this.Controls.Add(this.lblLivesLabel);
+            this.Name = "frmSimpleGame";
+            this.Text = "Simple Game";
+            this.Load += new System.EventHandler(this.frmSimpleGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgPaddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBall)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLivesLabel;
+        private System.Windows.Forms.Label lblScoreLabel;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLife;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.PictureBox imgPaddle;
+        private System.Windows.Forms.PictureBox imgBall;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Simple Game Program/Simple Game Program/frmSimpleGame.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Game_Program
+{
+    /*
+     * Simple Game
+     * This program will allow the player to return a ball and continue to earn points
+     * If the ball passes the paddle the player losees a point up to three times
+     */
+
+    public partial class frmSimpleGame : Form
+    {
+        int x;      // x is the positon on the x axis from the upper left corner
+        int y;      // y is the positon on the y axis from the upper left corner
+        int dx;     // speed is the change of x
+        int dy;     // speed is the change of y
+        int ptx;    // x coordinate of the paddle
+        int pty;    // y coordinate of the paddle
+        int score;  // how many times the ball hits the paddle
+        int life;   // number of lives starting at 3
+
+
+        public frmSimpleGame()
+        {
+            InitializeComponent();
+            KeyDown += new KeyEventHandler(frmSimpleGame_KeyDown);
+        }
+
+        private void frmSimpleGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            int px = imgPaddle.Location.X;
+            int py = imgPaddle.Location.Y;
+
+            if (e.KeyCode == Keys.Right) px += 5;
+            else if (e.KeyCode == Keys.Left) px -= 5;
+
+            if (px < 0)
+            {
+                px = 0;
+            }
+            if (px > this.ClientSize.Width - 50)
+            {
+                px = this.ClientSize.Width - 50;
+            }
+            imgPaddle.Location = new Point(px, py);
+        }
+
+
+        private void frmSimpleGame_Load(object sender, EventArgs e)
+        {
+            // Loads the ball on the screen at the upper left corner of the window
+            x = 1;      // x coordinate of the ball starting point
+            y = 1;      // y coordinate of the ball starting point
+            dx = 1;     // Speed of the ball in the x direction is 1
+            dy = 1;     // Speed of the ball in the y direction is 1
+            score = 0;  // The initial score is 0
+            life = 3;   // The initial life is 3
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            imgBall.Location = new Point(x, y);
+            x = x + dx;
+            if (x < 0)
+            {
+                dx = -dx; // if y is less than 0 then we change direction
+            }
+            else if (x + 10 > this.ClientSize.Width)
+            {
+                dx = -dx; // if y + 10, the radius of the circle is greater than the form height then we change direction
+            }
+
+            y = y + dy;
+            if (y < 0)
+            {
+                dy = -dy; // if y is less than 0 then we change direction
+            }
+
+            // checks if the ball hits the paddle
+            Point point1 = this.imgPaddle.Location;
+            ptx = (point1.X);
+            pty = (point1.Y);
+            if (x >= ptx)
+            {
+                if (x <= (ptx + 50))
+                {
+                    if ((y + 10) > pty)
+                    {
+                        dy = -dy;
+                        score = score + 1;
+                        lblScore.Text = Convert.ToString(score);
+                    }
+                    //trying to increment ball speed after hitting paddle   if (x = x + dx == dx, dy - 1)
+                    //{
+                    //    dx = dx + 10;
+                    //    dy = dy + 10;
+                    //}
+                }
+            }
+
+            // checks if the ball goes by the paddle imaginary line
+            if ((y + 9) > pty)
+            {
+                life = life - 1;
+                lblLife.Text = Convert.ToString(life);
+                dx = 1;
+                dy = 1;
+                x = 1;
+                y = 1;
+            }
+
+            // checks if the game is over
+            if (life == 0)
+            {
+                lblGameOver.Text = "Game Over";
+                x = -10;
+                y = -10;
+
+            }
+
+            // refresh the window
+            this.Invalidate();
+        }
+
+        private void imgBall_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
