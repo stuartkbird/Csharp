4d1f503f2b9c66a49ed715239479d46017613e88
Stu <49402927+stuartkbird@users.noreply.github.com>
2019-04-08 16:44:23 +0100

    Helicopter game

---
+++ Helicopter game/Helicopter game/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Helicopter_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pillar1 = new System.Windows.Forms.PictureBox();
+            this.pillar2 = new System.Windows.Forms.PictureBox();
+            this.ufo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::Helicopter_game.Properties.Resources.Helicopter;
+            this.player.Location = new System.Drawing.Point(0, 0);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 54);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // pillar1
+            // 
+            this.pillar1.Image = global::Helicopter_game.Properties.Resources.pillar;
+            this.pillar1.Location = new System.Drawing.Point(338, -6);
+            this.pillar1.Name = "pillar1";
+            this.pillar1.Size = new System.Drawing.Size(75, 185);
+            this.pillar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pillar1.TabIndex = 1;
+            this.pillar1.TabStop = false;
+            // 
+            // pillar2
+            // 
+            this.pillar2.Image = global::Helicopter_game.Properties.Resources.pillar;
+            this.pillar2.Location = new System.Drawing.Point(595, 246);
+            this.pillar2.Name = "pillar2";
+            this.pillar2.Size = new System.Drawing.Size(56, 146);
+            this.pillar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar2.TabIndex = 2;
+            this.pillar2.TabStop = false;
+            // 
+            // ufo
+            // 
+            this.ufo.BackColor = System.Drawing.Color.Transparent;
+            this.ufo.Image = global::Helicopter_game.Properties.Resources.alien1;
+            this.ufo.Location = new System.Drawing.Point(277, 208);
+            this.ufo.Name = "ufo";
+            this.ufo.Size = new System.Drawing.Size(68, 72);
+            this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ufo.TabIndex = 3;
+            this.ufo.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "00";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gametick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(797, 372);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ufo);
+            this.Controls.Add(this.pillar2);
+            this.Controls.Add(this.pillar1);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Helicopter Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pillar1;
+        private System.Windows.Forms.PictureBox pillar2;
+        private System.Windows.Forms.PictureBox ufo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Helicopter game/Helicopter game/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Helicopter_game
+{
+    public partial class Form1 : Form
+    {
+
+        // enter the variables
+        bool goup; // this is a boolean to allow player to go up
+        bool godown; // this is a boolean to allow player to go down
+        bool shot = false; // this will check if the player has shot any bullets
+        int score = 0; //this is a integer for player to keep score
+        int speed = 2; // this is the speed of the obstacles and ufos
+        Random rand = new Random(); //this is the random class to generate a random number
+        int playerSpeed = 7; // this integer will control how fast the player moves
+        int index; // this is a empty integer which will be used to change the ufo images
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                // if the player has pressed the up key
+                // we change the go up to true
+                goup = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                // if the player has pressed the down key
+                // we change the go down to true
+                godown = true;
+            }
+            if (e.KeyCode == Keys.Space && shot == false)
+            {
+                // if the player has pressed the space key
+                // AND shot boolean is false when they did
+                // then we run the make bullet function
+                // and change the shot from false to true
+                makeBullet();
+                shot = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                // if the player has pressed the up key
+                // change go up to false
+                goup = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                // if the player has pressed the down key
+                // change go down to false
+                godown = false;
+            }
+            if (shot == true)
+            {
+                // if shot variable is true
+                // we change it false so the player will have to shoot again
+                // for more bullets
+                shot = false;
+            }
+        }
+
+        private void gametick(object sender, EventArgs e)
+        {
+            // move pillar 1 towards the left of the screen
+            pillar1.Left -= speed;
+
+            // move pillar 2 towards the left of the screen
+            pillar2.Left -= speed;
+
+            // move ufo towards the left of the screen
+            ufo.Left -= speed;
+
+            // show the score on label 1
+            label1.Text = "Score: " + score;
+
+            if (goup)
+            {
+                // if go up is true then move the player up the screen
+                // notice its minue equals means it will deduct from the top location
+                // thus moving the player upwards
+                player.Top -= playerSpeed;
+            }
+
+            if (godown)
+            {
+                // if go down is true then move the player down the screen
+                // notice its plus equals means it will add to the top location
+                // thus moving the player downwards
+                player.Top += playerSpeed;
+            }
+
+            if (pillar1.Left < -150)
+            {
+                // if pillar 1 has gone past -150 which is off the screen
+                // then move it to 900 pixels to the right of the screen
+                // it will appear to have a continuous motion from right to left
+                pillar1.Left = 900;
+            }
+
+            if (pillar2.Left < -150)
+            {
+                // if pillar 2 has gone past -150 which is off the screen
+                // then move it to 1000 pixels to the right of the screen
+                // it will appear to have a continuous motion from right to left
+                pillar2.Left = 1000;
+            }
+
+            // the two || symbols represent the OR option in If statements
+            // the below if statement is logically checking the following
+            // if UFO has left the screen towards the left
+            // OR
+            // player has collided with the ufo object on screen
+            // OR
+            // player has collided with pillar 1 object
+            // OR
+            // player has collided with pillar 2 object
+            // then follow the instructions inside the statement
+            // we are able to check multiple conditions at the if statement
+
+            if (ufo.Left < -5 ||
+                player.Bounds.IntersectsWith(ufo.Bounds) ||
+                player.Bounds.IntersectsWith(pillar1.Bounds) ||
+                player.Bounds.IntersectsWith(pillar2.Bounds))
+            {
+                // if one of the above is true then we stop the timer
+                gameTimer.Stop();
+                // the game will show the final score to the player in a message box
+                MessageBox.Show("You failed the mission, you killed " + score + " Ufo's");
+            }
+
+            // below is a for loop thats checking the components in this form
+            // first we created a variable called X in this form
+            // x will be linked to the bullet object
+            // it will find out if the bullet object exists
+
+            foreach (Control X in this.Controls)
+            {
+                // if X is a picture box object AND it has a tag of bullet
+                // then we will follow the instructions within
+
+                if (X is PictureBox && X.Tag == "bullet")
+                {
+                    // move x towards the right of the screen
+                    X.Left += 15;
+
+                    // if x has left the screen towards the right
+                    // x's location is greater than 900 pixels from the screen
+                    if (X.Left > 900)
+                    {
+                        // then remove x from display
+                        this.Controls.Remove(X);
+
+                        // dispose the x from the application
+                        // we use the dispose method so it doesn't leak memory later on
+                        X.Dispose();
+                    }
+
+                    // below we will check if x collides with the ufo object
+                    if (X.Bounds.IntersectsWith(ufo.Bounds))
+                    {
+                        // if x collides with the ufo object
+
+                        // add 1 to the score
+                        score += 1;
+
+                        // remove the bullet from the screen
+                        this.Controls.Remove(X);
+
+                        // dispose the bullet from the program
+                        X.Dispose();
+
+                        // move the ufo object 1000 pixels off the screen
+                        ufo.Left = 1000;
+
+                        // generate a random vertical location for the ufo
+                        ufo.Top = rand.Next(5, 330) - ufo.Height;
+
+                        // run the; change ufo function it appears like a different ufo
+                        changeUFO();
+                    }
+                }
+            }
+        }
+
+        private void changeUFO()
+        {
+            index += 1; // increase index by 1
+
+            if (index > 3)
+            {
+                // if indexes value is greater than 3
+                // set it back to 1
+                index = 1;
+            }
+
+            // we will use the switch statement to switch between alien images
+            // by using the number in index we can switch them effectively
+            // when the numbers in index change this switch statement will follow
+            switch (index)
+            {
+                // if the number in index is 1
+                // then we will show the alien 1 skin on ufo picture box
+                case 1:
+                    ufo.Image = Properties.Resources.alien1;
+                    break;
+                // if the number in index is 2
+                // then we will show the alien 2 skin on ufo picture box
+                case 2:
+                    ufo.Image = Properties.Resources.alien2;
+                    break;
+                // if the number in index is 3
+                // then we will show the alien 3 skin on ufo picture box
+                case 3:
+                    ufo.Image = Properties.Resources.alien3;
+                    break;
+            }
+        }
+
+        private void makeBullet()
+        {
+            PictureBox bullet = new PictureBox();
+            // create a new picture box class to the form
+
+            bullet.BackColor = System.Drawing.Color.DarkOrange;
+            // set the colour of the bullet to dark orange
+
+            bullet.Height = 5;
+            // set bullet height to 5 pixels
+
+            bullet.Width = 10;
+            // set bullet width to 10 pixels
+
+            bullet.Left = player.Left + player.Width;
+            // bullet will place in front of player object
+
+            bullet.Top = player.Top + player.Height / 2;
+            // bullet will be middle of player object
+
+            bullet.Tag = "bullet";
+            // set the tag for the object to bullet
+
+            this.Controls.Add(bullet);
+            // finally adding the picture box bullet to the scene
+        }
+    }
+}
